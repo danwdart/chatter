6b52bf089c953a603e08d7b28b56a8f045804ea6
Dan Dart <git@dandart.co.uk>
2021-02-09 21:34:24 +0000

    remove stack, upgrade cabal and nix

--- default.nix
+++ default.nix
@@ -1,12 +1,35 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8102" }:
+{ nixpkgs ? import <nixpkgs> {},
+  compiler ? "ghc8102" }:
 let
+  gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
   myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
       discord-haskell = self.callCabal2nix "discord-haskell" (builtins.fetchGit {
         url = "https://github.com/aquarial/discord-haskell.git";
         rev = "8e1988edaf9b39cc27f44c966e16a33d1ead7a35";
       }) {};
+      chatter = self.callCabal2nix "chatter" (gitignore ./.) {};
     };
   };
+  shell = myHaskellPackages.shellFor {
+    packages = p: [
+      p.chatter
+    ];
+    buildInputs = [
+      nixpkgs.haskellPackages.cabal-install
+      nixpkgs.wget
+      nixpkgs.haskellPackages.stack
+      nixpkgs.haskellPackages.ghcid
+      nixpkgs.haskellPackages.stylish-haskell
+      nixpkgs.haskellPackages.hlint
+    ];
+    # withHoogle = true;
+  };
+  exe = nixpkgs.haskell.lib.justStaticExecutables (myHaskellPackages.chatter);
 in
-myHaskellPackages.callPackage ./chatter.nix {}
+{
+  inherit shell;
+  inherit exe;
+  inherit myHaskellPackages;
+  chatter = myHaskellPackages.chatter;
+}
