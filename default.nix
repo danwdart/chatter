b85cb432fd534dd40c7edd41406df39a108fb466
Dan Dart <git@dandart.co.uk>
2021-09-17 19:31:03 +0000

    upgrade to ghc 9

--- default.nix
+++ default.nix
@@ -1,9 +1,13 @@
 { nixpkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {},
-  compiler ? "ghc8107" }: # basement doesn't yet like 901
+  compiler ? "ghc901" }:
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
   myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
+      # 1.8.4-1.8.7 are broken
+      discord-haskell = self.callHackage "discord-haskell" "1.8.3" {};
+      # Depends on cabal-un-published http-client versions.
+      req = nixpkgs.pkgs.haskell.lib.doJailbreak (self.callHackage "req" "3.9.1" {});
       chatter = self.callCabal2nix "chatter" (gitignore ./.) {};
     };
   };
