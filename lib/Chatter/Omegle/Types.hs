2f544e051373e4da18940496da775f9b2dfa5ecb
Dan Dart <git@dandart.co.uk>
2023-06-09 20:14:57 +0100

    fail in omegle types

--- lib/Chatter/Omegle/Types.hs
+++ lib/Chatter/Omegle/Types.hs
@@ -29,23 +29,22 @@ data Event = Event {
     eventBody :: Message
 } deriving stock (Eq, Generic, Show)
 
--- @TODO whatever `fail` is in FromJSON
 instance FromJSON Event where
     parseJSON = \case
         (Array a) -> case V.toList a of
             [String a'] -> pure . Event a' $ Message []
             [String a', String b] -> pure . Event a' $ Message [b]
             [String a', Array b] -> pure . Event a' $ Message (strToVal <$> V.toList b)
-            [String _, String _, String _] -> error ("Triple value error" :: String)
+            [String _, String _, String _] -> fail ("Triple value error" :: String)
             (String a':xs) -> if a' == "statusInfo" then
                     pure . Event a' $ Message []
                 else
-                    error $ "Array is wrong" <> show xs
-            _ -> error ("Unknown array" :: String)
-        _ -> error ("Not array" :: String)
+                    fail $ "Array is wrong" <> show xs
+            _ -> fail ("Unknown array" :: String)
+        _ -> fail ("Not array" :: String)
         where
             strToVal (String e) = e
-            strToVal _          = error ("Not a string" :: String)
+            strToVal _          = fail ("Not a string" :: String)
 
 data LoginResponse = LoginResponse {
     clientID :: Text,
