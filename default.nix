30e13cf0a587bf9c38e4cf1fc29863db9f64aa60
Dan Dart <git@dandart.co.uk>
2024-11-10 23:34:05 +0000

    weeder + proper shell hook

--- default.nix
+++ default.nix
@@ -21,6 +21,10 @@ let
     packages = p: [
       p.chatter
     ];
+    shellHook = ''
+      gen-hie > hie.yaml
+      for i in $(find -type f | grep -v dist-newstyle); do krank $i; done
+    '';
     buildInputs = tools.defaultBuildTools ++ (with nixpkgs; [
       expect
       rlwrap
